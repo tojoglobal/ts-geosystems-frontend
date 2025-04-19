f3f8f30afd41ac8f7c0b3c637e5d495cd165a6f0
swapnilahmedshishir <ahmed66882298@gmail.com>
2025-04-19 14:47:56 +0600

    update the home part

--- src/Navbars/HeaderBottom.jsx
+++ src/Navbars/HeaderBottom.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const HeaderBottom = () => {
-  return <div>HeaderBottom</div>;
+  return (
+    <div className="bg-[#2c333a] p-2 text-center">
+      <p className="text-sm text-white italic font-bold">
+        We Offer Very Competitive Hire Rates On The Leica RTC360 3D Laser
+        Scanner - <span> Contact Us Today...</span>
+      </p>
+    </div>
+  );
 };
 
 export default HeaderBottom;
