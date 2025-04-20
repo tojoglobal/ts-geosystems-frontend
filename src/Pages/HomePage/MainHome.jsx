e5189e4ed9d9d2f1d86de8537f92a277aa4a742a
Momen <touristmomen@gmail.com>
2025-04-20 09:55:46 +0600

    OurYoutube mobile responsive and OurAdServices component design added

--- src/Pages/HomePage/MainHome.jsx
+++ src/Pages/HomePage/MainHome.jsx
@@ -1,10 +1,12 @@
 import Banner from "./Banner";
+import OurAdServices from "./OurAdServices";
 import OurYoutube from "./OurYoutube";
 import PopularBrands from "./PopularBrands";
 const MainHome = () => {
     return (
         <>
             <Banner />
+            <OurAdServices />
             <PopularBrands />
             <OurYoutube />
         </>
