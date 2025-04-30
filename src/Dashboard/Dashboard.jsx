6720e24152d02fbc2886c194d50eacc037530537
Momen <touristmomen@gmail.com>
2025-04-30 11:43:08 +0600

    HomePageControl component for admin

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -10,6 +10,7 @@ import { Card, CardContent } from "@/Components/ui/card";
 import DashboardCharts from "./DBComponents/DashboardCharts";
 import DashboardWidgets from "./DBComponents/DashboardWidgets";
 import LatestTransactions from "./DBComponents/LatestTransactions";
+import HomePageControl from "./Control/HomePageControl";
 
 export default function Dashboard() {
   return (
@@ -54,9 +55,11 @@ export default function Dashboard() {
       <div className="col-span-4">
         <DashboardWidgets />
       </div>
-
       <div className="col-span-4">
         <LatestTransactions />
+      </div>
+      <div className="col-span-4">
+        <HomePageControl />
       </div>
     </div>
   );
