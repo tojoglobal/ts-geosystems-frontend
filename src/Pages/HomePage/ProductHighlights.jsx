dedaa6a1895f06080c97bc38d8e2608474cb39ff
Momen <touristmomen@gmail.com>
2025-05-18 16:39:10 +0600

    product highlight dynamic data added

--- src/Pages/HomePage/ProductHighlights.jsx
+++ src/Pages/HomePage/ProductHighlights.jsx
@@ -5,173 +5,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { Link } from "react-router-dom";
-
-const featured = [
-  {
-    name: "Leica iCON iCG70 GNSS RTK Rover Leica iCON iCG70 GNSS RTK Rover",
-    img: "https://ts-geosystems.com.bd/assets/images/f6ID2zYF.png",
-    img2: "https://ts-geosystems.com.bd/assets/images/JMqcf5wY.png",
-    price: "100,000",
-    discountPrice: "120,000",
-    productUrl:
-      "https://ts-geosystems.com.bd/product/Kolida-KTS470-Windows-Total-Station",
-  },
-  {
-    name: "Featured Product 2",
-    img: "https://ts-geosystems.com.bd/assets/images/JMqcf5wY.png",
-    img2: "https://ts-geosystems.com.bd/assets/images/f6ID2zYF.png",
-    price: "110,000",
-    discountPrice: "130,000",
-    productUrl:
-      "https://ts-geosystems.com.bd/product/Kolida-KTS470-Windows-Total-Station",
-  },
-  {
-    name: "Featured Product 3",
-    img: "https://ts-geosystems.com.bd/assets/images/f6ID2zYF.png",
-    img2: "https://ts-geosystems.com.bd/assets/images/JMqcf5wY.png",
-    price: "105,000",
-    discountPrice: "125,000",
-    productUrl:
-      "https://ts-geosystems.com.bd/product/Kolida-KTS470-Windows-Total-Station",
-  },
-  {
-    name: "Featured Product 1",
-    img: "https://ts-geosystems.com.bd/assets/images/f6ID2zYF.png",
-    img2: "https://ts-geosystems.com.bd/assets/images/JMqcf5wY.png",
-    price: "100,000",
-    discountPrice: "120,000",
-    productUrl:
-      "https://ts-geosystems.com.bd/product/Kolida-KTS470-Windows-Total-Station",
-  },
-  {
-    name: "Featured Product 2",
-    img: "https://ts-geosystems.com.bd/assets/images/JMqcf5wY.png",
-    img2: "https://ts-geosystems.com.bd/assets/images/f6ID2zYF.png",
-    price: "110,000",
-    discountPrice: "130,000",
-    productUrl:
-      "https://ts-geosystems.com.bd/product/Kolida-KTS470-Windows-Total-Station",
-  },
-  {
-    name: "Featured Product 3",
-    img: "https://ts-geosystems.com.bd/assets/images/f6ID2zYF.png",
-    img2: "https://ts-geosystems.com.bd/assets/images/JMqcf5wY.png",
-    price: "105,000",
-    discountPrice: "125,000",
-    productUrl:
-      "https://ts-geosystems.com.bd/product/Kolida-KTS470-Windows-Total-Station",
-  },
-];
-
-const topSellers = [
-  {
-    name: "Top Seller 1",
-    img: "https://ts-geosystems.com.bd/assets/images/JMqcf5wY.png",
-    img2: "https://ts-geosystems.com.bd/assets/images/f6ID2zYF.png",
-    price: "95,000",
-    discountPrice: "115,000",
-    productUrl:
-      "https://ts-geosystems.com.bd/product/Kolida-KTS470-Windows-Total-Station",
-  },
-  {
-    name: "Top Seller 2",
-    img: "https://ts-geosystems.com.bd/assets/images/f6ID2zYF.png",
-    img2: "https://ts-geosystems.com.bd/assets/images/f6ID2zYF.png",
-    price: "98,000",
-    discountPrice: "118,000",
-    productUrl:
-      "https://ts-geosystems.com.bd/product/Kolida-KTS470-Windows-Total-Station",
-  },
-  {
-    name: "Top Seller 3",
-    img: "https://ts-geosystems.com.bd/assets/images/JMqcf5wY.png",
-    img2: "https://ts-geosystems.com.bd/assets/images/f6ID2zYF.png",
-    price: "99,500",
-    discountPrice: "119,500",
-    productUrl:
-      "https://ts-geosystems.com.bd/product/Kolida-KTS470-Windows-Total-Station",
-  },
-  {
-    name: "Top Seller 1",
-    img: "https://ts-geosystems.com.bd/assets/images/JMqcf5wY.png",
-    img2: "https://ts-geosystems.com.bd/assets/images/f6ID2zYF.png",
-    price: "95,000",
-    discountPrice: "115,000",
-    productUrl:
-      "https://ts-geosystems.com.bd/product/Kolida-KTS470-Windows-Total-Station",
-  },
-  {
-    name: "Top Seller 2",
-    img: "https://ts-geosystems.com.bd/assets/images/f6ID2zYF.png",
-    img2: "https://ts-geosystems.com.bd/assets/images/f6ID2zYF.png",
-    price: "98,000",
-    discountPrice: "118,000",
-    productUrl:
-      "https://ts-geosystems.com.bd/product/Kolida-KTS470-Windows-Total-Station",
-  },
-  {
-    name: "Top Seller 3",
-    img: "https://ts-geosystems.com.bd/assets/images/JMqcf5wY.png",
-    price: "99,500",
-    discountPrice: "119,500",
-    productUrl:
-      "https://ts-geosystems.com.bd/product/Kolida-KTS470-Windows-Total-Station",
-  },
-];
-
-const newProducts = [
-  {
-    name: "New Product 1",
-    img: "https://ts-geosystems.com.bd/assets/images/f6ID2zYF.png",
-    img2: "https://ts-geosystems.com.bd/assets/images/f6ID2zYF.png",
-    price: "102,000",
-    discountPrice: "122,000",
-    productUrl:
-      "https://ts-geosystems.com.bd/product/Kolida-KTS470-Windows-Total-Station",
-  },
-  {
-    name: "New Product 2",
-    img: "https://ts-geosystems.com.bd/assets/images/JMqcf5wY.png",
-    img2: "https://ts-geosystems.com.bd/assets/images/f6ID2zYF.png",
-    price: "108,000",
-    discountPrice: "128,000",
-    productUrl:
-      "https://ts-geosystems.com.bd/product/Kolida-KTS470-Windows-Total-Station",
-  },
-  {
-    name: "New Product 3",
-    img: "https://ts-geosystems.com.bd/assets/images/f6ID2zYF.png",
-    img2: "https://ts-geosystems.com.bd/assets/images/f6ID2zYF.png",
-    price: "106,000",
-    discountPrice: "126,000",
-    productUrl:
-      "https://ts-geosystems.com.bd/product/Kolida-KTS470-Windows-Total-Station",
-  },
-  {
-    name: "New Product 1",
-    img: "https://ts-geosystems.com.bd/assets/images/f6ID2zYF.png",
-    price: "102,000",
-    discountPrice: "122,000",
-    productUrl:
-      "https://ts-geosystems.com.bd/product/Kolida-KTS470-Windows-Total-Station",
-  },
-  {
-    name: "New Product 2",
-    img: "https://ts-geosystems.com.bd/assets/images/JMqcf5wY.png",
-    price: "108,000",
-    discountPrice: "128,000",
-    productUrl:
-      "https://ts-geosystems.com.bd/product/Kolida-KTS470-Windows-Total-Station",
-  },
-  {
-    name: "New Product 3",
-    img: "https://ts-geosystems.com.bd/assets/images/f6ID2zYF.png",
-    price: "106,000",
-    discountPrice: "126,000",
-    productUrl:
-      "https://ts-geosystems.com.bd/product/Kolida-KTS470-Windows-Total-Station",
-  },
-];
+import useDataQuery from "../../utils/useDataQuery";
 
 const tabOptions = [
   { name: "Featured Products", key: "featured" },
@@ -181,21 +15,43 @@ const tabOptions = [
 
 const ProductHighlights = () => {
   const [activeTab, setActiveTab] = useState("featured");
-  const [items, setItems] = useState(featured);
+  const [items, setItems] = useState([]);
   const swiperRef = useRef(null);
   const [isBeginning, setIsBeginning] = useState(true);
   const [isEnd, setIsEnd] = useState(false);
 
+  const {
+    data = {},
+    isLoading,
+    error,
+  } = useDataQuery(["productHighlights"], "/api/product-highlights");
+  const highlightsData = data?.data;
+  console.log(highlightsData);
+
   useEffect(() => {
-    if (activeTab === "featured") setItems(featured);
-    else if (activeTab === "top") setItems(topSellers);
-    else if (activeTab === "new") setItems(newProducts);
-  }, [activeTab]);
+    if (!isLoading && !error) {
+      if (activeTab === "featured") setItems(highlightsData.featured || []);
+      else if (activeTab === "top") setItems(highlightsData.top || []);
+      else if (activeTab === "new") setItems(highlightsData.new || []);
+    }
+  }, [activeTab, highlightsData, isLoading, error]);
 
   const handleSlideChange = (swiper) => {
     setIsBeginning(swiper.isBeginning);
     setIsEnd(swiper.isEnd);
   };
+
+  if (isLoading) {
+    return <div className="max-w-[1370px] mx-auto py-12">Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div className="max-w-[1370px] mx-auto py-12">
+        Error loading product highlights
+      </div>
+    );
+  }
 
   return (
     <div className="max-w-[1370px] mx-auto rounded-md px-3 md:px-0 py-6 md:py-12 bg-white relative">
@@ -248,7 +104,7 @@ const ProductHighlights = () => {
       <div className="mt-4">
         <Swiper
           modules={[Navigation]}
-          spaceBetween={8}
+          spaceBetween={9}
           slidesPerView={4}
           slidesPerGroup={4}
           onSwiper={(swiper) => {
@@ -283,47 +139,70 @@ const ProductHighlights = () => {
           {items.map((item, idx) => (
             <SwiperSlide key={idx}>
               <div className="relative flex flex-col items-center bg-white h-full">
-                {activeTab !== "featured" && (
-                  <p className="absolute top-2 right-2 px-2 py-[2px] rounded-md text-white bg-[#e62245] z-50">
+                {item?.sale === 1 && (
+                  <p className="absolute top-1 right-3 px-2 py-[1px] font-bold rounded-[4px] text-white text-[14px] bg-[#e62245] z-50">
                     SALE
                   </p>
                 )}
-                <Link to={item?.productUrl}>
+                <Link
+                  to={`/product/${item.product_name
+                    .replace(/\s+/g, "-")
+                    .toLowerCase()}`}
+                >
                   <div className="relative group w-full max-w-[200px] md:max-w-[260px] mx-auto">
-                    <img
-                      src={item?.img}
-                      alt={item.name}
-                      className="w-full h-auto transition-opacity duration-300 group-hover:opacity-0"
-                    />
-                    <img
-                      src={item?.img2}
-                      alt={`${item?.name} hover`}
-                      className="w-full h-auto absolute top-0 left-0 transition-opacity duration-300 opacity-0 group-hover:opacity-100"
-                    />
+                    {item.image_urls &&
+                      JSON.parse(item.image_urls).length > 0 && (
+                        <>
+                          <img
+                            src={`${import.meta.env.VITE_OPEN_APIURL}${
+                              JSON.parse(item.image_urls)[0]
+                            }`}
+                            alt={item.product_name}
+                            className="w-full h-[256.19px] transition-opacity duration-300 group-hover:opacity-0"
+                          />
+                          {JSON.parse(item.image_urls).length > 1 && (
+                            <img
+                              src={`${import.meta.env.VITE_OPEN_APIURL}${
+                                JSON.parse(item.image_urls)[1]
+                              }`}
+                              alt={`${item.product_name} hover`}
+                              className="w-full h-[256.19px] absolute top-0 left-0 transition-opacity duration-300 opacity-0 group-hover:opacity-100"
+                            />
+                          )}
+                        </>
+                      )}
                   </div>
                 </Link>
                 <div className="w-full border-t border-gray-200 mt-3 pt-2 flex flex-col flex-grow space-y-1">
                   <p className="text-xs text-gray-700">
-                    Total Station | Sku: 65dVv8Jr8fe
+                    {item.category ? JSON.parse(item.category).cat : "Category"}{" "}
+                    | Sku: {item.sku || "N/A"}
                   </p>
-                  <div className="min-h-[46px] flex items-start">
+                  <div className="flex items-start">
                     <Link
-                      to={item?.productUrl}
+                      to={`/product/${item.product_name
+                        .replace(/\s+/g, "-")
+                        .toLowerCase()}`}
                       className="font-semibold text-sm text-gray-600 hover:text-[#e62245]"
                     >
-                      {item.name}
+                      {item.product_name}
                     </Link>
                   </div>
                   <div className="space-x-2">
-                    <span className="text-sm font-bold text-[#222]">
-                      ৳{item.price}
-                    </span>
-                    <span className="text-xs line-through text-gray-400">
-                      ৳{item.discountPrice}
-                    </span>
+                    <p className="flex items-center gap-2 text-sm font-bold text-[#222]">
+                      <span> ৳{item.price}</span>
+                      <span className="underline text-gray-400 text-sm">
+                        (Ex. VAT)
+                      </span>
+                    </p>
+                    {item.discountPrice && (
+                      <span className="text-xs line-through text-gray-400">
+                        ৳{item.discountPrice}
+                      </span>
+                    )}
                   </div>
                   <div className="flex items-center gap-1 text-sm text-[#b3b3b5]">
-                    ৳11994.00 <span className="underline">(Inc. VAT)</span>
+                    ৳{item.price} <span className="underline">(Inc. VAT)</span>
                   </div>
                   <button className="mt-1 cursor-pointer bg-[#e62245] hover:bg-[#c91d3a] text-white text-sm font-semibold py-1.5 px-4 rounded w-full transition-colors duration-200">
                     ADD TO CART
