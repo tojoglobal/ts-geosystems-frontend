da7006c7671f325774a5cd70ae95211dc3dc9879
Momen <touristmomen@gmail.com>
2025-06-21 21:44:10 +0600

    RichTextRenderer fixed with old code

--- src/utils/RichTextRenderer.jsx
+++ src/utils/RichTextRenderer.jsx
@@ -94,3 +94,12 @@ const RichTextRenderer = ({ html }) => {
 };
 
 export default RichTextRenderer;
+
+// const RichTextRenderer = ({ html }) => {
+//   if (!html) return null;
+
+//   return (
+//     <div
+//       className="prose prose-sm sm:prose-base max-w-none text-black bg-white p-4 rounded shadow [&_ul]:pl-5 [&_ul]:list-disc [&_strong]:text-black [&_b]:text-black"
+//       dangerouslySetInnerHTML={{ __html: html }}
+//     />
